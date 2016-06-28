f128bdf8f3f7c60e2971162d5e705322af138552
Chyld Medford <chyld.medford@gmail.com>
2016-06-27 23:02:11 -0500

    Update server.js

--- src/server.js
+++ src/server.js
@@ -14,6 +14,7 @@ app.set('views', path.join(__dirname, '../views'));
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, HEAD');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
